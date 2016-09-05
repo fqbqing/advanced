512d99961353c2fdbb1e1640822f64440f139d61
aqing <yappy2000@gmail.com>
2016-09-05 17:10:27 +0800

    user modules 删除

--- backend/modules/user/views/admin/_menu.php
+++
@@ -1,4 +0,0 @@
-<?= \yii\widgets\Menu::widget(['options' => ['class' => 'nav nav-pills nav-stacked'],'items' => [
-    ['label' => '账户','url' => ['/user/profile']],
-    ['label' => '信息','url' => ['/user/profile']],
-]]) ?>
