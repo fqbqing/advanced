3683f257ba728c570210d3b02061b2dd8469f707
aqing <yappy2000@gmail.com>
2016-09-19 11:01:38 +0800

    common/main.php文件配置 1、定义全局runtime目录 2、配置时间戳格式化

--- common/config/main.php
+++ common/config/main.php
@@ -1,13 +1,22 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
-
+    //定义全局runtime目录
+    'runtimePath' => '@root/runtime',
     'language' => 'zh-CN',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
-
+        //配置时间戳格式化
+        'formatter' => [
+            'dateFormat' => 'yyyy-MM-dd',
+            'datetimeFormat' => 'yyyy-MM-dd HH:mm:ss',
+            'timeFormat' => 'HH:mm:ss',
+            'decimalSeparator' => '.',
+            'thousandSeparator' => ' ',
+            'currencyCode' => 'CNY',
+        ],
         'config' => [ //动态配置
             'class' => 'common\\components\\Config',
             'localConfigFile' => '@common/config/main-local.php'
